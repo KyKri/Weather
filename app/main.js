f217650ce93183d76cf89b668f35e5c7bc4b43af
Kyle Krick <krkrick@gmail.com>
2017-03-31 23:31:00 -0700

    Built app without errors..

--- app/main.js
+++ app/main.js
@@ -2,5 +2,5 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstraoModule(app_module_1.AppModule);
+platform.bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map

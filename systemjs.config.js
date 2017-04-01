59d1d820ad9e4635127cbe6079c1fc527b9cdb7a
Kyle Krick <krkrick@gmail.com>
2017-04-01 16:02:17 -0700

    Fixed typo from commone..

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,12 +1,12 @@
 (function(global){
     System.config({
         paths: {
-            'npm': 'node_modules/'
+            'npm:': 'node_modules/'
         },
         map: {
             app: 'app',
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-            '@angular/commone': 'npm:@angular/common/bundles/common.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',

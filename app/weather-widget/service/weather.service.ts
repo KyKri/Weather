6e1bae12ea9d07c8e5595a091207d6c19f954f47
Kyle Krick <krkrick@gmail.com>
2017-06-11 16:01:29 -0700

    Added code to get current location

--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -2,4 +2,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 
-export class WeatherService{  }
+export class WeatherService{ 
+    getCurrentLocation(): [number, number]{
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(pos => {
+                console.log("Position: ", pos.coords.latitude, ",",  pos.coords.longitude); //TODO: remove
+                return [pos.coords.latitude, pos.coords.longitude];
+            },
+            err => console.error("Unable to get position - ", err));
+        } else {
+            console.error("Geolocation is not available");
+            return [0, 0];
+        }
+    }
+}

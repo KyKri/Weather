d73151b7b5bd22defac1fc547f63ed68f23d4d57
Kyle Krick <krkrick@gmail.com>
2017-07-29 13:56:04 -0700

    Added google maps api key and url

--- app/weather-widget/constants/constants.js
+++ app/weather-widget/constants/constants.js
@@ -1,4 +1,6 @@
 "use strict";
 exports.FORECAST_KEY = "d42d2796707bbd03fff1b360d3cdce42";
 exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
+exports.GEO_KEY = 'AIzaSyCOTF0Q934o65X4E_f3roeUX-k42D3nX8o';
+exports.GEO_ROOT = 'https://maps.googleapis.com/maps/geocode/json';
 //# sourceMappingURL=constants.js.map

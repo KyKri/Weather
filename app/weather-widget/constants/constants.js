d82853aa849fc6ab721ca1cd090b86d2e11483a6
Kyle Krick <krkrick@gmail.com>
2017-07-29 20:28:03 -0700

    Updated google maps url

--- app/weather-widget/constants/constants.js
+++ app/weather-widget/constants/constants.js
@@ -2,5 +2,5 @@
 exports.FORECAST_KEY = "d42d2796707bbd03fff1b360d3cdce42";
 exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
 exports.GEO_KEY = 'AIzaSyCOTF0Q934o65X4E_f3roeUX-k42D3nX8o';
-exports.GEO_ROOT = 'https://maps.googleapis.com/maps/geocode/json';
+exports.GEO_ROOT = 'https://maps.googleapis.com/maps/api/geocode/json';
 //# sourceMappingURL=constants.js.map
--- app/weather-widget/constants/constants.ts
+++ app/weather-widget/constants/constants.ts
@@ -1,4 +1,4 @@
 export const FORECAST_KEY = "d42d2796707bbd03fff1b360d3cdce42";
 export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
 export const GEO_KEY = 'AIzaSyCOTF0Q934o65X4E_f3roeUX-k42D3nX8o';
-export const GEO_ROOT = 'https://maps.googleapis.com/maps/geocode/json';
+export const GEO_ROOT = 'https://maps.googleapis.com/maps/api/geocode/json';
